bc8ed23f9bfbe74886ecdcba980526a0eabec4c4
Anas1Ashkar <anas_ashker_2000_ps@yahoo.com>
2023-08-08 00:52:50 +0300

    score?

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_new_enhancements/screens/new_quiz.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      title: 'Quiz app',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
